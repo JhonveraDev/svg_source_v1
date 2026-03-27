3b4c46f1dad0c3d2422b9813836bf7e07af72328
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-03-27 14:15:32 -0500

    responsive in sidebar

--- src/layout/sidebar/sidebar.links.ts
+++ src/layout/sidebar/sidebar.links.ts
@@ -5,6 +5,6 @@ export const sidebarLinks: SidebarLink[] = [
   { to: "/", label: "Home", icon: HomeIcon },
   { to: "/favorites", label: "Favorites", icon: FavoritesIcon },
   { to: "/docs/api", label: "API", icon: ApiIcon },
-  { to: "/docs/shadcn-ui", label: "Shadcn/ui", icon: ShadcnIcon },
+  // { to: "/docs/shadcn-ui", label: "Shadcn/ui", icon: ShadcnIcon },
   { to: "/extensions", label: "Extensions", icon: ExtensionsIcon },
 ];
